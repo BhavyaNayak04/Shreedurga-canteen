1c7687f20d58af0cbbb6ba6638448a9cc89acee8
Yuvraj Singh <125214828+yuvi-636@users.noreply.github.com>
2024-01-24 00:32:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-const menuIcon = document.getElementById("menuIcon");
-const dropdownContent = document.getElementById("dropdownContent");
+const menuIcon = document.querySelector(".menu-icon");
 
-menuIcon.addEventListener("mouseenter", () => {
-  dropdownContent.classList.add("show-dropdown");
+menuIcon.addEventListener("click", () => {
+  dropdownContent.classList.toggle("show-dropdown");
+  if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+    window.location.href = "menu.html";
+  }
 });
-
-menuIcon.addEventListener("mouseleave", () => {
-  dropdownContent.classList.remove("show-dropdown");
-});
-
-dropdownContent.addEventListener("mouseenter", () => {
-  dropdownContent.classList.add("show-dropdown");
-});
-
-dropdownContent.addEventListener("mouseleave", () => {
-  dropdownContent.classList.remove("show-dropdown");
+document.addEventListener("click", (event) => {
+  const isClickInside = dropdownContent.contains(event.target) || menuIcon.contains(event.target);
+  if (!isClickInside) {
+    dropdownContent.classList.remove("show-dropdown");
+  }
 });
