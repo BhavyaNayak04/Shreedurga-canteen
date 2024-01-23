eed9cb099326968606d7107d91638a10a532f063
Yuvraj Singh <125214828+yuvi-636@users.noreply.github.com>
2024-01-24 02:55:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menuIcon = document.querySelector(".menu-icon");
+const dropdownContent = document.querySelector(".dropdown-content");
 
 menuIcon.addEventListener("click", () => {
   if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
@@ -7,8 +8,10 @@ menuIcon.addEventListener("click", () => {
     dropdownContent.classList.toggle("show-dropdown");
   }
 });
+
 document.addEventListener("click", (event) => {
   const isClickInside = dropdownContent.contains(event.target) || menuIcon.contains(event.target);
+
   if (!isClickInside) {
     dropdownContent.classList.remove("show-dropdown");
   }
